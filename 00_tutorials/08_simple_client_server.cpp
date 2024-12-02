5472876b1b0029c98b92c7e7667929e0aacc48d3
vu3.nguyen <vu3.nguyen@lge.com>
2024-12-02 14:09:22 +0900

    update

--- 00_tutorials/08_simple_client_server.cpp
+++ 00_tutorials/08_simple_client_server.cpp
@@ -79,8 +79,55 @@ void run_client()
     while (true)
     {
         printf("Client is ready to create new request\n");
+        char request_buffer[MESSAGE_SIZE];
+        memset(request_buffer, 0, MESSAGE_SIZE);
+
+        printf("Enter command: ");
+        fgets(request_buffer, MESSAGE_SIZE, stdin);
+        printf("Request: %s\n", request_buffer);
+
+        int request_buffer_len = strlen(request_buffer);
+        int total_sent_bytes = 0;
+        while (total_sent_bytes < request_buffer_len)
+        {
+            int sent_bytes = send(sock_client, request_buffer + total_sent_bytes, request_buffer_len - total_sent_bytes, 0);
+            if (sent_bytes <= 0)
+            {
+                break;
+            }
+
+            total_sent_bytes += sent_bytes;
+        }
+
+        if (total_sent_bytes < request_buffer_len)
+        {
+            report_error("Client sent request fail");
+            continue;
+        }
+
+        char response_buffer[MESSAGE_SIZE];
+        memset(response_buffer, 0, MESSAGE_SIZE);
+        int total_received_bytes = 0;
+        int received_bytes = recv(sock_client, response_buffer + total_received_bytes, MESSAGE_SIZE - total_received_bytes, 0);
+        while (received_bytes > 0)
+        {
+            total_received_bytes += received_bytes;
+            received_bytes = recv(sock_client, response_buffer + total_received_bytes, MESSAGE_SIZE - total_received_bytes, 0);
+        }
+
+        printf("Response: %s\n", response_buffer);
+
+        if (strcmp(request_buffer, "exit") == 0
+         || strcmp(request_buffer, "quit") == 0
+         || strcmp(request_buffer, "shutdown") == 0)
+        {
+            break;
+        }
+
         sleep(1);
     }
+
+    printf("Client exit with normal status\n");
 }
 
 int main(int argc, char** argv)
