6497f3ced192224f1c256785a555c873fc9fbd71
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2024-12-12 15:48:18 +0000

    update

--- 01_networking_libraries/openssl/src/https_client.cpp
+++ 01_networking_libraries/openssl/src/https_client.cpp
@@ -12,6 +12,13 @@
 #include <errno.h>
 #include <sys/poll.h>
 
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+#define TCP_PROTOCOL_NAME "tcp"
+#define REQUEST_SIZE 1024
+#define RESPONSE_SIZE 4096
+
 void print_usage(const char *program_name)
 {
     fprintf(stderr, "Usage: %s <hostname> <port>\n", program_name);
@@ -22,8 +29,154 @@ void report_error(const char* message)
     fprintf(stderr, "Error: %s\n", message);
 }
 
-void perform_https_request(char* hostname, int port)
+void print_sockaddr_info(sockaddr *sa)
 {
+    char ip[INET6_ADDRSTRLEN];
+    memset(ip, 0, INET6_ADDRSTRLEN);
+
+    void *addr;
+    int port;
+
+    if (sa->sa_family == AF_INET)
+    {
+        struct sockaddr_in *sin = (struct sockaddr_in *)sa;
+        addr = &(sin->sin_addr);
+        port = ntohs(sin->sin_port);
+    }
+    else if (sa->sa_family == AF_INET6)
+    {
+        struct sockaddr_in6 *sin6 = (struct sockaddr_in6 *)sa;
+        addr = &(sin6->sin6_addr);
+        port = ntohs(sin6->sin6_port);
+    }
+    else
+    {
+        report_error("Unknown address family");
+        return;
+    }
+
+    if (inet_ntop(sa->sa_family, addr, ip, sizeof(ip)) == NULL)
+    {
+        report_error("inet_ntop() failed");
+        return;
+    }
+
+    printf("%s:%d\n", ip, port);
+}
+
+void perform_https_request(char* hostname, char* port)
+{
+    int rc;
+
+    protoent* tcp_proto = getprotobyname(TCP_PROTOCOL_NAME);
+    if (tcp_proto == NULL)
+    {
+        report_error("TCP protocol is not supported");
+        return;
+    }
+
+    addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = tcp_proto->p_proto;
+    addrinfo* addr_server;
+    rc = getaddrinfo(hostname, port, &hints, &addr_server);
+    if (rc != 0)
+    {
+        report_error("Resolve address failed");
+        return;
+    }
+
+    int sock_client = socket(addr_server->ai_family, addr_server->ai_socktype, addr_server->ai_protocol);
+    if (sock_client < 0)
+    {
+        report_error("socket() failed");
+        return;
+    }
+
+    for (addrinfo* p_server = addr_server; p_server != NULL; p_server = p_server->ai_next)
+    {
+        print_sockaddr_info(p_server->ai_addr);
+        rc = connect(sock_client, p_server->ai_addr, p_server->ai_addrlen);
+        if (rc == 0)
+        {
+            break;
+        }
+    }
+
+    if (rc != 0)
+    {
+        report_error("connect() failed");
+        return;
+    }
+
+    SSL_load_error_strings();
+    OpenSSL_add_ssl_algorithms();
+
+    const SSL_METHOD* ssl_method = TLS_client_method();
+    SSL_CTX* ssl_context = SSL_CTX_new(ssl_method);
+    if (ssl_context == NULL)
+    {
+        report_error("Unable to create SSL context");
+        freeaddrinfo(addr_server);
+        close(sock_client);
+        EVP_cleanup();
+        return;
+    }
+
+    SSL* ssl = SSL_new(ssl_context);
+    if (ssl == NULL)
+    {
+        report_error("SSL_new() failed");
+        return;
+    }
+
+    SSL_set_fd(ssl, sock_client);
+    rc = SSL_connect(ssl);
+    if (rc <= 0)
+    {
+        report_error("SSL_connect() failed");
+        ERR_print_errors_fp(stderr);
+        return;
+    }
+
+    printf("SSL connection is done with cipher suite %s\n", SSL_get_cipher(ssl));
+
+    char http_request[REQUEST_SIZE];
+    memset(http_request, 0, REQUEST_SIZE);
+    sprintf(http_request, "GET / HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n", hostname);
+
+    rc = SSL_write(ssl, http_request, strlen(http_request));
+    if (rc <= 0)
+    {
+        report_error("SSL_write() failed");
+        ERR_print_errors_fp(stderr);
+        return;
+    }
+
+    char http_response[RESPONSE_SIZE + 1];
+    memset(http_response, 0, RESPONSE_SIZE);
+    int received_bytes = 0;
+    int total_received_bytes = 0;
+    do
+    {
+        received_bytes = SSL_read(ssl, http_response + total_received_bytes, RESPONSE_SIZE - total_received_bytes);
+        if (received_bytes > 0)
+        {
+            printf("Received %d bytes\n", received_bytes);
+            total_received_bytes += received_bytes;
+        }
+    } while (received_bytes > 0);
+
+    http_response[total_received_bytes] = 0;
+    printf("%s\n", http_response);
+
+    freeaddrinfo(addr_server);
+    close(sock_client);
+    SSL_free(ssl);
+    SSL_CTX_free(ssl_context);
+    EVP_cleanup();
 }
 
 int main(int argc, char** argv)
@@ -34,7 +187,7 @@ int main(int argc, char** argv)
         return -1;
     }
 
-    perform_https_request(argv[1], atoi(argv[2]));
+    perform_https_request(argv[1], argv[2]);
 
     return 0;
 }
